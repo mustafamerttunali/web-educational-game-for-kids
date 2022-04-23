64b68dae798e4ed8bb3823addda5caaca99595ad
Mustafa Mert Tunalı <tunalim@mef.edu.tr>
2022-04-24 00:11:07 +0300

    Install Boostrap, Add Footer, Organize files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Components
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// Boostrap 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div>
+        <Container>
+          <Row>
+              <Col md ={12}>
+                <p className="text-center"> <small>&copy; Copyright {currentYear} - Mustafa Mert Tunali, Ahmet Yildiz, Emir Çetin Memis</small> </p> 
+              </Col>
+          </Row>
+        </Container>
+    </div>
+  )
+}
