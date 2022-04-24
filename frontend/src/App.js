a3ff71914cd89ec1c6c23565d482ba05b47fade0
Mustafa Mert Tunalı <tunalim@mef.edu.tr>
2022-04-24 17:06:39 +0300

    Add Routes & Init Home Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
