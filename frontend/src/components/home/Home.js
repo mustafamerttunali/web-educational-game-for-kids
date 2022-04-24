e645e4472a32a3ee41ca0fa37c3b93276f8fd8b0
Mustafa Mert Tunalı <tunalim@mef.edu.tr>
2022-04-25 00:22:33 +0300

    Token check for frontend

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Home() {
+ 
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if(!token) {
+      window.location.href = '/login';
+    }
+  }, []);
+
   return (
     <div>Home</div>
   )
